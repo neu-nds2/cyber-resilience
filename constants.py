ec163c2aa341276c76c35b95e0278af5dd63c495
Simona Boboila <simona@Simonas-MacBook-Pro.local>
2023-06-22 17:14:15 -0400

    constants file

--- constants.py
+++ constants.py
@@ -1,6 +1,14 @@
+import sys
+import os
 
+# add the parent directory to the path
+sys.path.insert(0, os.path.abspath("../"))
 
-# the path to the data folder
-# this path may need to be changed to point correctly
-PATH_DIR = "../data"
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+PATH_DIR = os.path.join(dir_path, "sample_data_and_results")
+
+# name of the file containing  the input graph 
+GRAPH_FILE = "oregon1_010526.txt"
+
 
